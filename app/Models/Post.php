e907ee0ed5a5ad90db1c3002239c254b2b3555a3
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-03-30 07:21:52 +0700

    Praktikum 2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    protected $table = 'my_posts';
+    protected $primaryKey = 'my_id';
+    protected $keyType = 'string';
+    public $incrementing = false;
 }
