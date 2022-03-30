90ce07eff0ed2788947e2cc038c4804b73e82700
As'ad Muhaimin Iskandar <90304523+iska08@users.noreply.github.com>
2022-03-30 07:28:06 +0700

    Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,20 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*
-Route::get('/', function () {
-    return view('welcome');
-});
-*/
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/about', function () {
-    return view('about');
-});
+/*
 Route::get('/post', function () {
     return view('post');
+});
+*/
+Route::get('/about', function () {
+    return view('about');
 });
 Route::get('/contact', function () {
     return view('contact');
 });
+// Route::get('/post','PostController@post');
+Route::get('/post','PostController@index')->name('post');
